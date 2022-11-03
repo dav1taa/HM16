fffc040867ec6abbc59aa50e063fd9031ce983b8
dav1taa <nebieridze.dato12@gmail.com>
2022-11-03 17:29:52 +0400

    HM16

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 
 
-function
+function car (mark, model, year){
+    this.mark = mark;
+    this.model = model;
+    this.year = year;
+    this.getCarInfo = function() {
+        document.write("მარკა" + this.mark +  "მოდელი" + this.model + "released in" + this.year )
+
+    }
+}
+
+
+function person (name,age,cars) {
+    this.name = name;
+    this.age = age;
+    this.cars = car;
+    this.getPersonCar = function 
+
+}
+
+
+var tom = new person ("tom",26,)
