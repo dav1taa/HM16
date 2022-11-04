db2b0ef8acecfbd899a3fb46477b1135ee2b68e4
dav1taa <nebieridze.dato12@gmail.com>
2022-11-04 18:40:55 +0400

    HM16

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ function car (mark, model, year){
     this.model = model;
     this.year = year;
     this.getCarInfo = function() {
-        document.write("მარკა" + this.mark +  "მოდელი" + this.model + "released in" + this.year )
+        document.write("მარკა" + this.mark +  "მოდელი" + this.model + "released in" + this.year );
 
     }
 }
 
 
-function person (name,age,cars) {
+function person (name,age) {
     this.name = name;
     this.age = age;
-    this.cars = car;
-    this.getPersonCar = function 
+    this.driveCar = function (car) {
+        document.write (" dadis am manqanit"+ this.cars)
+    }
+    this.displayInfo = function (){
+        document.write ("saxeli" + this.name + "asaki" + this.age)
+    }
+
 
 }
 
 
-var tom = new person ("tom",26,)
+var tom = new person ("tom",26,"tesla");
+tom.displayInfo()
+
+var tesla = new car ("tesla", "model s", 2008);
+tom.driveCar(tesla);
